222db23115bf137398548c6c3e19a816f88a8965
Beatrizrva <bresendiz@atl.yearup.org>
2015-04-24 16:13:50 -0400

    Programming Project 1

---
+++ Project1/src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! I'm Beatriz. Welcome to the Java class.");
+		//* When I first took one of the braces, it asked me if I was sure if I wanted to run the command. I said I did, so when it finished running, red letters appeared in the console saying that I didn't close the command. *//
+		
+		
+		
+	}
+
+}
