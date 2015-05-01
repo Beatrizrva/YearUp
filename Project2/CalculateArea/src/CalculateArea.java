4826c8c7a659343c3133a46c98620745372156ec
Beatrizrva <bresendiz@atl.yearup.org>
2015-04-30 22:44:11 -0400

    This is project 2: CalculateArea.

---
+++ Project2/CalculateArea/src/CalculateArea.java
@@ -0,0 +1,23 @@
+
+public class CalculateArea 
+{
+	public static void main(String[] args) {
+	{ 
+		//Finding Area of Circle and Rectangle
+		double circleArea = CalculatecircleArea(2);
+		System.out.println("The area of a circle is: " + circleArea);		
+		double rectangleArea = (5 );
+		System.out.println("The area of the rectangle is " + rectangleArea);}		
+	}
+
+	static double CalculatecircleArea(int radius)
+	{
+		double area = 3.14 * radius * radius; 
+		return area;
+	}
+	CalculateArea(int length,int breadth)
+	{
+		double area = length * breadth;
+		return;
+	}
+}
